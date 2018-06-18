90142156b31dbdc28a77c0f62ea92c9d1965e4f7
mukundmurrali <mukundmurrali9@gmail.com>
2018-06-18 08:35:03 +0530

    Update TwoSum.java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -4,7 +4,7 @@ public class TwoSum {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 int num1 = nums[i];
-                int num2 = nums[j];
+                int num2 = nums[j]
                 if (num1 + num2 == target) {
                     return new int[] { i, j };
                 }
